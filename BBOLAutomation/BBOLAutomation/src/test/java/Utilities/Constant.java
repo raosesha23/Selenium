71dba222b975161db4deb35daa8a91a67e74544e
raosesha23 <64381280+raosesha23@users.noreply.github.com>
2020-08-30 12:46:43 +0100

    Add files via upload

---
+++ BBOLAutomation/BBOLAutomation/src/test/java/Utilities/Constant.java
@@ -0,0 +1,13 @@
+package Utilities;
+
+public class Constant {
+
+	/**Config Properties file **/
+	public final static String CONFIG_PROPERTIES_DIRECTORY = "Utilities\\config.properties";
+	
+	public final static String GECKO_DRIVER_DIRECTORY = System.getProperty("user.dir") + "\\src\\test\\java\\Utilities\\other\\geckodriver.exe";
+	
+	public final static String CHROME_DRIVER_DIRECTORY = System.getProperty("user.dir") + "\\src\\test\\java\\Utilities\\other\\chromedriver.exe";
+	
+	public final static String IE_DRIVER_DIRECTORY = System.getProperty("user.dir") + "\\src\\test\\java\\Utilities\\other\\IEDriverServer.exe";
+}
